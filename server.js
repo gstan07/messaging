53cf6b0c5c62d4a3ab80fe38cd333a292e72f312
George Aureliu Stan <gestan@adobe.com>
2016-02-24 23:15:00 +0200

    debugging

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ var default_connection = io.use(function(socket,next){
 	var client_key = handshake.app_key;
 	var handshake_error = "";
 	if(allowed_keys[client_key].allowed_hosts.indexOf(host) == -1){
+		console.log(host,client_key,allowed_hosts[client_key]);
 		handshake_error = "invalid app key";		
 	}
 	if(handshake_error == ""){
