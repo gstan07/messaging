4874801208e790e07e111d83ff563428c799d74d
George Aureliu Stan <gestan@adobe.com>
2016-02-24 12:49:38 +0200

    whitelisting fcs

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use('/', express.static(__dirname + '/public'));
 server.listen(port, function() { console.log('listening on port: '+port)});
 
 var allowed_keys = {
+	"fcsfcsfcs":{
+		allowed_hosts:["http://localhost:2000","http://fcsteaua.ro","http://www.fcsteaua.ro"]
+	}
 	"plmplmplm":{
 		allowed_hosts:["http://localhost:2000","http://flirting.chat","http://www.flirting.chat"]
 	}
